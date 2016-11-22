9e25770808919338cc5ff8e4c21bf6ec9ad39ce4
Jared Vasquez <jared.vasquez@yale.edu>
2016-11-22 01:57:02 -0500

    Improved install file (incomplete)

--- installReqs.sh
+++ installReqs.sh
@@ -1,15 +1,22 @@
 # Install pandamonium 
-git clone https://github.com/dguest/pandamonium.git
-
+install_pandamon() {
+  git clone https://github.com/dguest/pandamonium.git
+  newPATH="export PATH=\"$PWD/pandamonium:\$PATH\""
+  echo "Adding following line to your .bashrc"
+  echo $newPATH
+  echo "" >> ~/.bashrc
+  echo $newPATH >> ~/.bashrc
+  echo ""
+}
 
 # Create downloads folder
 #  --> needed to source xAODMerge
-mkdir pbslogs downloads
-cd downloads
-setupATLAS
-rcSetup Base,2.4.22
-rc find_packages
-rc compile
-
-# Return to initial direcory
-cd ../
+create_download() {
+  mkdir pbslogs download
+  cd download
+  setupATLAS
+  rcSetup Base,2.4.22
+  rc find_packages
+  rc compile
+  cd ../          #return to initial dir
+}
