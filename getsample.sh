5f9d3cbeb72f2ba5d17482c13f8a4667f932e214
Jared Vasquez <jared.vasquez@yale.edu>
2016-11-23 01:56:27 -0500

    Works for mc samples

--- getsample.sh
+++ getsample.sh
@@ -6,16 +6,23 @@
 #PBS -o pbslogs/$PBS_JOBNAME.o${PBS_JOBID}
 #PBS -j oe
 
+
 # Set your output directory
-outputDir='/group/atlas/data/jgv7/mxoad_hgamma/h014test'
+downloadDir='/group/atlas/data/jgv7/mxoad_hgamma/h014test'
+
+if [ ! -d "$downloadDir" ]; then
+  echo 'specified download directory $downloadDir does not exist! Please create/change.'
+  return 1
+fi
+
 
 # Prepare environment
 cd $PBS_O_WORKDIR
 cd download
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh --quiet
+rcSetup -q
 lsetup rucio
-env
 
 
 # Check sample exists 
@@ -30,16 +37,16 @@ echo -e "\t\t SUCCESS! Download will now start.\n"
 
 
 #Download sample to directory
-cmd="rucio get --ndownloader 3 $dsName --dir $outputDir"
+cmd="rucio get --ndownloader 3 $dsName --dir $downloadDir"
 echo $cmd
 $cmd
 
 
 # Check that all files were downloaded?
 echo -e "\nChecking that all files were downloaded..."
-files=$(echo $outputDir/$dsName/*)
+files=$(echo $downloadDir/$dsName/*)
 Nfiles=$(echo $files | awk '{print NF}')
-ls $outputDir/$dsName 
+ls $downloadDir/$dsName 
 if [ ! "$Nfiles" -eq "$NfilesGrid" ]; then
   echo "ERROR! Dataset $dsName did not download all files! Files on Grid: $NfilesGrid, Files locally: $Nfiles!"
   return 1
@@ -48,21 +55,24 @@ echo -e "\t\t SUCCESS!\n"
 
 
 # If all files are downloaded, merge sample
-newDSname="${dsName/group.phys-higgs./}"
-newDSname="${newDSname/_MxAOD.root/.root}"
-cmd="xAODMerge $outputDir/$newDSname $files"
-echo $cmd
-if $cmd; then
-  echo -e "\t\t xAODMerge SUCCESS!\n"
-else
-  echo -e "\t\t xAODMerge failed: Safe Merging... get comfortable, this takes some time.\n"
-  cmd2="./SafeMerge.sh $outputDir/$dsName"
-  echo $cmd2
-  $cmd2
+sampleName="${dsName/group.phys-higgs./}"
+sampleName="${sampleName/_MxAOD.root/.root}"
+if [[ $sampleName =~ 'group.phys-higgs.data*' ]]; then
+  echo "Sample is data, will not merge."
+  return 1
+else 
+  cmd="xAODMerge $downloadDir/$sampleName $files"
+  echo $cmd
+  if $cmd; then
+    echo -e "\t\t xAODMerge SUCCESS!\n"
+  else
+    echo -e "\t\t xAODMerge failed: Safe Merging... get comfortable, this takes some time.\n"
+    cmd2="./SafeMerge.sh $downloadDir/$dsName"
+    echo $cmd2
+    $cmd2
+  fi
 fi
-
 
 # Transfer merged sample to eos
 
-
-
+cd ../
