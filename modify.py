e08daae37b408975ba5be197ffb1c924fc34355d
Jared Vasquez <jared.vasquez@yale.edu>
2016-12-30 16:05:28 -0500

    Added modify script

---
+++ modify.py
@@ -0,0 +1,35 @@
+import sys, pickle
+
+__downloadDB = 'logdls.pkl'
+
+operation  = sys.argv[1]
+samplename = sys.argv[2]
+
+samplename = samplename.replace('_MxAOD.root','')
+if samplename[-1] != '/':
+  samplename += '/'
+
+#print 'Will run %s for sample %s' % (operation,samplename)
+try:
+  downloads = pickle.load( open( __downloadDB, 'rb' ) )
+except IOError:
+  downloads = {}
+
+dlsamples = { downloads[k][1]: k for k in downloads }
+
+def findSample( samplename ):
+  if samplename in dlsamples: return True
+  return False
+
+if operation == 'find':
+  if not findSample(samplename):
+    print 'Sample %s not found.' % samplename
+
+if operation == 'remove':
+  if findSample(samplename):
+    del downloads[dlsamples[samplename]]
+    del dlsamples[samplename]
+    if not findSample(samplename):
+      print 'Removed successfully.'
+      pickle.dump( downloads, open( __downloadDB, 'wb' ) )
+
